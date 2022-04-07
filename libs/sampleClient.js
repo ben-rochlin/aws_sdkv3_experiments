a662d4e6071edf4dca5aa77c0c044bdf617c36bd
ben-rochlin <brochlin91@gmail.com>
2022-04-07 16:50:50 +1000

    comments for later reference

--- libs/sampleClient.js
+++ libs/sampleClient.js
@@ -1,6 +1,8 @@
+//in the V3 of the aws SDK, it will automatically utilise credentials provided in the .env file.
 import 'dotenv/config'
 import { S3Client } from "@aws-sdk/client-s3";
 
+//becasue of the automatic credentials import all we need to provide to the S3 client is the region.
 const s3Client = new S3Client({region: "ap-southeast-2"});
 
 export {s3Client};
